c48703a29e9432e77baa72e4880ce3b6f1bbee42
Julien Eluard <julien.eluard@gmail.com>
2019-07-17 15:39:20 +0200

    Migrate to latest sticker contract

--- src/status_im/ethereum/contracts.cljs
+++ src/status_im/ethereum/contracts.cljs
@@ -8,7 +8,14 @@
    :status/tribute-to-talk
    {:testnet "0xC61aa0287247a0398589a66fCD6146EC0F295432"}
    :status/stickers
-   {:testnet "0x39d16CdB56b5a6a89e1A397A13Fe48034694316E"}})
+   {:testnet "0x8cc272396be7583c65bee82cd7b743c69a87287d"
+    :mainnet ""}
+   :status/sticker-market
+   {:testnet "0x6CC7274aF9cE9572d22DFD8545Fb8c9C9Bcb48AD"
+    :mainnet ""}
+   :status/sticker-pack
+   {:testnet "0xf852198d0385c4b871e0b91804ecd47c6ba97351"
+    :mainnet ""}})
 
 (defn get-address
   [db contract]
