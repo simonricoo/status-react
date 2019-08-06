41a909e74f052dc7bb03d61cebda02c253c476b2
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-08-06 18:16:51 +0200

    Upgrade react-native to 0.60.5
    - Use community net-info, react-native-webview instead of deprecated react-native classes
    - Remove react-native-tcp
    - Upgrade react-native libs (react-native-camera, react-native-firebase, react-native-mail, react-native-udp, react-native-webview-bridge)
    - Do not include `:react-native-android` module explicitly
    - Take advantage of RN AutoLinking
    - nix: Update Gradle dependencies

---
+++ react-native.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    dependencies: {
+        'react-native-dialogs': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
+        },
+        'react-native-status-keycard': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
+        },
+        'react-native-webview-bridge': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
+        },
+    },
+};
