ebbbb587a2aed06b58c922c6300b2b33240a4c9c
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2019-09-17 19:16:22 +0300

    Small fixes

--- react-native/src/desktop/status_im/react_native/js_dependencies.cljs
+++ react-native/src/desktop/status_im/react_native/js_dependencies.cljs
@@ -36,3 +36,5 @@
 (def react-native-shake     #js {})
 (def net-info               #js {:default #js {}})
 (def react-native-mail      #js {:mail (fn [])})
+(def async-storage          #js {})
+(def back-handler           #js {})
