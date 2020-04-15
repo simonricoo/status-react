5505ee87c53956e259859352b3a86c89ef72e09e
Gheorghe Pinzaru <feross95@gmail.com>
2020-04-15 14:48:35 +0300

    Change animated root ns

--- src/fiddle/components/animated.cljs
+++
@@ -1,9 +0,0 @@
-(ns fiddle.components.animated
-  (:require [reagent.core :as reagent]
-            [oops.core :refer [oget]]
-            [status-im.react-native.js-dependencies :as js-deps]))
-
-(def animated (oget js-deps/react-native-reanimated "default"))
-
-(def view (reagent/adapt-react-class (oget animated "View")))
-(def text (reagent/adapt-react-class (oget animated "Text")))
