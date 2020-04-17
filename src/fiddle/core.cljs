2ebef11b65e2a1204f07250ebe7bbad5d2081aa1
Gheorghe Pinzaru <feross95@gmail.com>
2020-04-17 18:44:01 +0300

    Add animated header component
    A view with header which can be animated on scroll, used in walled, profile and contact screens

--- src/fiddle/core.cljs
+++ src/fiddle/core.cljs
@@ -1,10 +1,12 @@
 (ns fiddle.core
   (:require [fiddle.components.text :as text]
+            [fiddle.components.safe-area :as safe-area]
             [fiddle.components.header :as header]
-            [fiddle.components.safe-area :as safe-area]))
+            [fiddle.components.animated-header :as animated-header]))
 
 (def text text/text)
 (def header header/header)
+(def animated-header animated-header/header)
 
 (def safe-area-provider safe-area/provider)
 (def safe-area-consumer safe-area/consumer)
